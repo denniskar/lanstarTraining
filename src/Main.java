e921983122ffb7b9ff98ffba2ac8478b0b53a701
Rose1845 <odhiamborose466@gmail.com>
2023-05-09 19:56:18 +0300

    simple java program to model a car dealership

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         Main main=new Main();
         main.train(6,9);
+        RCar car1 = new RCar("Toyota", "Camry", 25000.0, 2020);
+       RCar car2 = new RCar("Ford", "Mustang", 30000.0, 2019);
+       RCar car3 = new RCar("Chevrolet", "Corvette", 70000.0, 2021);
+
+        car1.printDetails();
+        car2.printDetails();
+        car3.printDetails();
+
+        System.out.println("Discounting the price of the Chevrolet Corvette by 10%...");
+        car3.discount(10.0);
+        car3.printDetails();
     }
 
     public  void train(int i, int i1){
