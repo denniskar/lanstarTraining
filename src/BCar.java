81ce12c9db7f6ded7ca1888bb0a389d5d0755f0c
briller <briller254@gmail.com>
2023-05-10 11:13:06 +0300

    bcar done

---
+++ src/BCar.java
@@ -0,0 +1,16 @@
+public class BCars {
+    //instance variables
+
+    private String make;
+    private String model;
+    private double price;
+    private int year;
+    
+    
+    //car constructor
+public Bcars(String)
+
+
+
+
+}
